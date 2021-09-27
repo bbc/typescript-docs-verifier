0ca47f0a4c14175e5048997e086cef6829b7c42a
Paul Douglas Brimicombe <paul.brimicombe@bbc.co.uk>
2021-09-27 12:38:40 +0100

    Remove transpiled javascript from repository

--- dist/src/CodeBlockExtractor.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CodeBlockExtractor {
-    static readonly TYPESCRIPT_CODE_PATTERN: RegExp;
-    private constructor();
-    static extract(markdownFilePath: string): Promise<string[]>;
-    private static readFile;
-    private static extractCodeBlocksFromMarkdown;
-}
